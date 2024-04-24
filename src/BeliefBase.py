f8dc0b604c92de66d7ff71b20c89a0328ada7877
Zedrichu <adrzvizdencojr@gmail.com>
2024-04-24 13:47:25 +0200

    Implemented entrenchment contraction finally
    Co-authored-by: JepMik <jepmik@live.dk>
    Co-authored-by: kubejk

--- src/BeliefBase.py
+++ src/BeliefBase.py
@@ -1,5 +1,3 @@
-from sympy import symbols
-
 from Clause import Clause
 from utils import *
 from typing import List, Optional
@@ -8,11 +6,9 @@ from src.Belief import Belief
 
 class BeliefBase:
     _beliefs: List[Belief]
-    order: int
 
     def __init__(self, beliefs: Optional[List[Belief]] = None):
         self._beliefs = []
-        self.order = 0
         if beliefs:
             for belief in beliefs:
                 self.add_belief(belief)
@@ -21,10 +17,22 @@ class BeliefBase:
         return self._beliefs
 
     def add_belief(self, belief: Belief):
-        # Probably we should also assign how old the belief is
         self._beliefs.append(belief)
-        belief.order = self.order
-        self.order += 1
+
+    def update_entrenchment(self):
+        for belief in self._beliefs:
+            epistemic_value = self.epistemic(belief)
+            belief.update_priority(epistemic_value)
+
+    @staticmethod
+    def epistemic(belief: Belief) -> float:
+        # Tautologies are most entrenched
+        empty_base = BeliefBase([Belief('~'+belief.formula)])
+        if empty_base.resolution():
+            return 1
+
+        num_literals = len(belief.literals)
+        return 1 / (num_literals + 1)
 
     def remove_belief(self, belief: Belief):
         self._beliefs.remove(belief)
@@ -47,6 +55,50 @@ class BeliefBase:
             clause = Clause(set(dissociate(disjunction, Or)))
             clausal_form.add(clause)
         return clausal_form
+
+    def resolution(self):
+        """
+        Resolution refutation algorithm for propositional logic
+        :return: bool
+
+        Pseudo-code:
+        clauses = set of clauses in CNF form of KB and ¬α
+        new = {}
+        while True:
+            for each pair of clauses Ci, Cj in clauses:
+                resolvents = resolve(Ci, Cj)
+                if resolvents contains the empty clause:
+                    return True
+                new = new ∪ resolvents
+            if new ⊆ clauses:
+                return False
+            clauses = clauses ∪ new
+        """
+
+        # Split the base into conjunctions of clauses - set of clauses
+        clauses = self.clausal_form()
+
+        # Apply resolution refutation
+        while True:
+            new_clauses = set()
+            # Attempt to resolve each pair of clauses
+            for clause1 in clauses:
+                for clause2 in clauses:
+                    # Set of all possible resolvents if clauses are clashing
+                    resolvents = Clause.resolve(clause1, clause2)
+                    # If the empty clause is found, the formula is entailed (refutation is unsatisfiable)
+                    if Clause.empty_clause() in resolvents:
+                        # Printout for debugging
+                        # for r in resolvents:
+                        #     print(r)
+                        return True
+                    # Add the resolvents to the new set of clauses
+                    new_clauses.update(resolvents)
+            # No new resolvent were found - formula is not entailed (refutation is satisfiable)
+            if new_clauses.issubset(clauses):
+                return False
+            # Union of the new resolvents with the set of clauses in the base
+            clauses.update(new_clauses)
 
     def __str__(self):
         return (f'BeliefBase|\n\t'
--- src/Entailment.py
+++ src/Entailment.py
@@ -1,5 +1,6 @@
 import copy
-from sympy import symbols, to_cnf
+
+from sympy import to_cnf
 
 from Clause import Clause
 from utils import *
@@ -14,60 +15,11 @@ def entails(premise_base: BeliefBase, statement: str) -> bool:
     :param statement: statement to be entailed
     :return: boolean value if the statement is entailed by the premise base
     """
-    return resolution(premise_base, to_cnf(statement))
+    tmp_base = copy.copy(premise_base)
+    # Refute the query belief - negation of the statement
+    tmp_base.add_belief(Belief('~' + statement))
 
-"""
-Propositional logic resolution pseudoalgorithm
-
-clauses = set of clauses in CNF form of KB and ¬α
-new = {}
-while True:
-    for each pair of clauses Ci, Cj in clauses:
-        resolvents = resolve(Ci, Cj)
-        if resolvents contains the empty clause:
-            return True
-        new = new ∪ resolvents
-    if new ⊆ clauses:
-        return False
-    clauses = clauses ∪ new
-"""
-
-
-def resolution(base: BeliefBase, alpha: BooleanFunction):
-    """
-    Resolution refutation algorithm for propositional logic
-    :param base: BeliefBase
-    :param alpha: BooleanFunction
-    :return: bool
-    """
-    # Create a new belief base with the premise base and the negation of the formula
-    belief_base = copy.copy(base)
-    belief_base.add_belief(Belief(~alpha))
-
-    # Split the base into conjunctions of clauses - set of clauses
-    clauses = belief_base.clausal_form()
-
-    # Apply resolution refutation
-    while True:
-        new_clauses = set()
-        # Attempt to resolve each pair of clauses
-        for clause1 in clauses:
-            for clause2 in clauses:
-                # Set of all possible resolvents if clauses are clashing
-                resolvents = Clause.resolve(clause1, clause2)
-                # If the empty clause is found, the formula is entailed (refutation is unsatisfiable)
-                if Clause.empty_clause() in resolvents:
-                    # Printout for debugging
-                    # for r in resolvents:
-                    #     print(r)
-                    return True
-                # Add the resolvents to the new set of clauses
-                new_clauses.update(resolvents)
-        # No new resolvent were found - formula is not entailed (refutation is satisfiable)
-        if new_clauses.issubset(clauses):
-            return False
-        # Union of the new resolvents with the set of clauses in the base
-        clauses.update(new_clauses)
+    return tmp_base.resolution()
 
 
 if __name__ == '__main__':
